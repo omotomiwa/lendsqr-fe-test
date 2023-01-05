0701334322c8453cd774dc062b01df44832a48bf
omotomiwa <42933279+omotomiwa@users.noreply.github.com>
2023-01-05 10:39:09 +0100

    rectifying deployment errors

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import {BrowserRouter as Router} from "react-router-dom" 
+//import {BrowserRouter as Router} from "react-router-dom" 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
